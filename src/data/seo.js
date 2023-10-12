a365a2b8a086cae741f25da07674de04b6be91fe
tegarerik41 <76865589+tegarerik41@users.noreply.github.com>
2023-10-12 22:35:32 +0700

    Upload Vercel
    PortofolioKU

---
+++ src/data/seo.js
@@ -0,0 +1,38 @@
+const SEO = [
+	{
+		page: "home",
+		description:
+			"Welcome to my web portfolio, where you can explore a range of projects and accomplishments in the fields of Data Engineering, Data Analysis, and Data Science. I am a data enthusiast, dedicated to unraveling the world of data, unearthing insights from hidden information, and designing data-backed solutions. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+		keywords: ["Tegar", "Tegar Erik", "Tegar Erik Alianto"],
+	},
+
+	{
+		page: "about",
+		description:
+			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
+			keywords: ["Tegar", "Tegar Erik", "Tegar Erik Alianto"],
+	},
+
+	{
+		page: "articles",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+			keywords: ["Tegar", "Tegar Erik", "Tegar Erik Alianto"],
+	},
+
+	{
+		page: "projects",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to.",
+			keywords: ["Tegar", "Tegar Erik", "Tegar Erik Alianto"],
+	},
+
+	{
+		page: "contact",
+		description:
+			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
+			keywords: ["Tegar", "Tegar Erik", "Tegar Erik Alianto"],
+	},
+];
+
+export default SEO;
---
+++ src/data/user.js
@@ -0,0 +1,83 @@
+const INFO = {
+	main: {
+		title: "Portofolio Tegar Erik Alianto",
+		name: "Tegar Erik",
+		email: "tegarerik04@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://instagram.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Data Engineer, Data Analyst, Data Scientist, and  MLBB Player.",
+		description:
+			"Welcome to my web portfolio, where you can explore a range of projects and accomplishments in the fields of Data Engineering, Data Analysis, and Data Science. I am a data enthusiast, dedicated to unraveling the world of data, unearthing insights from hidden information, and designing data-backed solutions. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+	},
+
+	about: {
+		title: "I’m Tegar Erik Alianto. I live in Yogyakarta Indonesia, where I design the future.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, big data, and more.",
+	},
+
+	projects: [
+		{
+			title: "Project 1",
+			description:
+				"Sebuah proyek untuk mendeteksi penyakit pada daun padi.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com/tegarerik41/Deteksi-Daun-Padi",
+		},
+
+		{
+			title: "Project 2",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 3",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 4",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 5",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
